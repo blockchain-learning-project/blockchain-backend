60a8d7280ce444524d608f62b1bc2ef52f5163f3
Siddhesh <saddy97531@gmail.com>
2022-08-27 17:26:09 +0530

    final

--- contracts/Insurance.sol
+++ contracts/Insurance.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
+
+/*
+    We have used different DataDisaster which is a bit modified by us.
+*/
 pragma solidity ^0.8.7;
 
 interface DisasterInterface  {
@@ -23,24 +27,25 @@ contract Insurance
         bool locked;
     }
     
-    bool public contractLock;
-    uint256 public startDay;
-    uint256 public endDay;
+    uint256 private totalBalance;
+    bool private contractLock;
+    uint256 private startDay;
+    uint256 private endDay;
 
-    farmer[] public list_farmer;
-    mapping(address => uint256) public list_farmer_index;
-    mapping(address => bool) public list_farmer_valid;
+    farmer[] private list_farmer;
+    mapping(address => uint256) private list_farmer_index;
+    mapping(address => bool) private list_farmer_valid;
 
-    mapping(uint256 => uint256) public day_aggregate;
+    mapping(uint256 => uint256) private day_aggregate;
 
     // Calculating sum( x(i) * y(i) ),
     // tried to reduce time complexity form O(n * m) to O(n),
     // where n => number of days from start to end & m => numbers of farmers.
-    uint256 public totalDayFactorAggregate;
+    uint256 private totalDayFactorAggregate;
 
-    uint256 public totalSeverityFactorAggregate;
+    uint256 private totalSeverityFactorAggregate;
 
-    DisasterInterface DisasterInterfaceObject;
+    DisasterInterface private DisasterInterfaceObject;
     modifier unlocked
     {
         require(contractLock == false, "Contract has been locked");
@@ -57,17 +62,17 @@ contract Insurance
     {
         contractLock = false;
         startDay = getDay();
-        DisasterInterfaceObject = DisasterInterface(0x9D7f74d0C41E726EC95884E0e97Fa6129e3b5E99);
+        DisasterInterfaceObject = DisasterInterface(0x814a6f7dB849f0443B8e5C21bCD034a722FD3E4e);
     }
-
+/*
     function getFarmer() public view returns(farmer memory)
     {
         uint256 farmer_index = list_farmer_index[msg.sender];
 
         return list_farmer[farmer_index];
     }
-
-
+*/
+/*//used for testing.
     // !!! remove this before submission
     uint256 public c_day = 0;
     function setDay(uint256 day) public
@@ -78,12 +83,11 @@ contract Insurance
     {
         return c_day;
     } 
-
-/* 
+*/
     function getDay() private view returns(uint256)
     {
         return (block.timestamp) / 5 minutes;
-    } */
+    }
 
     function registerFarmer(string memory district_) public unlocked
     {
@@ -114,6 +118,8 @@ contract Insurance
         t_farmer.total_paid_premium += msg.value;
 
         day_aggregate[current_day] += msg.value;
+
+        totalBalance += msg.value;
     }
     
     function calculateAggregateDayFactor() private view returns (uint256)
@@ -132,7 +138,6 @@ contract Insurance
         return aggregate_day_factor;
     }
 
-    //flawed => 10 0 50 0 0 will be calculated as 10 50
     function calculateDayFactor(farmer memory t_farmer) private view returns (uint256)
     {
         uint256 t_day_factor = 0;
@@ -162,10 +167,34 @@ contract Insurance
         uint denominator
     );
 
+    function getTotalAccumulatedSeverity() private view returns (uint256)
+    {
+        uint256 aggregate = 0;
+        for (uint256 i=0;i< list_farmer.length;i++)
+        {
+            aggregate += DisasterInterfaceObject.getAccumulatedSeverity(list_farmer[i].district);
+        }
+        return aggregate;
+    }
+
+    function canClaim() private view returns (bool){
+        uint currDate = getDay();
+        uint256 diff = currDate - startDay;
+        diff = (((diff/60)/60)/24);
+        if(diff > 3 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    event payout(uint insurancePayment);
     function claimInsurance() public payable
     {
+        require(canClaim() == true, "Can't claim now wait 3 years.");
         uint256 today = getDay();
         farmer memory t_farmer = list_farmer[list_farmer_index[msg.sender]];
+        require(t_farmer.locked == false, "Can't claim again.");
 
         if(contractLock != true)
         {
@@ -173,7 +202,7 @@ contract Insurance
             endDay = today;
             
             totalDayFactorAggregate = calculateAggregateDayFactor();
-            totalSeverityFactorAggregate = DisasterInterfaceObject.getTotalAccumulatedSeverity();
+            totalSeverityFactorAggregate = getTotalAccumulatedSeverity();
         }
 
         uint256 day_factor = calculateDayFactor(t_farmer);
@@ -181,13 +210,15 @@ contract Insurance
 
         uint256 p1 = totalSeverityFactorAggregate * day_factor;
         uint256 p2 = totalDayFactorAggregate * severity_factor;
-        uint256 numerator = (p1 + p2) * t_farmer.total_paid_premium;
+        uint256 numerator = (p1 + p2) * totalBalance;
         uint256 denominator = (2 * totalDayFactorAggregate * totalSeverityFactorAggregate);
 
-        emit cnumber(endDay, totalDayFactorAggregate, totalSeverityFactorAggregate, day_factor, severity_factor, p1, p2, numerator, denominator);
+        //emit cnumber(endDay, totalDayFactorAggregate, totalSeverityFactorAggregate, day_factor, severity_factor, p1, p2, numerator, denominator);
 
         uint256 total_insurance_amount = numerator / denominator;
+        emit payout(total_insurance_amount);
 
         payable(t_farmer.farmer_address).transfer(total_insurance_amount);
+        t_farmer.locked = true;
     }
 }
